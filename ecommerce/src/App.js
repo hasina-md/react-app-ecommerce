f86769b544749650ddcdd1417103c15ebcc8fc19
hasina.md shaik <mohammadhaseena71@gmail.com>
2023-08-30 07:26:29 +0000

    app router created

---
+++ ecommerce/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AppRouter from './AppRouter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>PRODUCTS OF ECOMMERCE APP</h1>
+      <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/AppRouter.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+export default function AppRouter() {
+    return <div>sample router</div>
+}
